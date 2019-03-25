8652394547f887881c5e30e65dbd33b82c3fef9b
Anton Liuliavskyi <antonliuliawskyi@gmail.com>
2019-03-25 16:43:49 +0100

    Initial Commit

---
+++ ChecklistsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChecklistsApp
+//
+//  Created by Test on 25/03/2019.
+//  Copyright © 2019 Peanch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
